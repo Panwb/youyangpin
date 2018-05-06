1d2227d56227c7a57e652225231a0b440aeabe96
jay <jaypan1988@gmail.com>
2018-05-06 08:04:53 +0800

    .

--- youyangpin/src/views/applyrecord/applyrecord.vue
+++ youyangpin/src/views/applyrecord/applyrecord.vue
@@ -45,8 +45,8 @@
                                         <div class="topbox">
                                             <div class="date">{{ item.datetime }}</div><!--todo-->
                                             <div class="shop"><span :class="item.ShopType=='淘宝店'?'icon icon-tao':'icon icon-tian'"></span>{{ item.ShopName }}</div>
-                                            <div class="wechat">申请时间:{{ formatDate(item.CreateTime, 'hms') }}</div>
-                                            <div class="phone">审核时间:{{ formatDate(item.CheckTime, 'hms') }}</div>
+                                            <div class="wechat">申请时间：{{ formatDate(item.CreateTime, 'hms') }}</div>
+                                            <div class="phone">审核时间：{{ formatDate(item.CheckTime, 'hms') }}</div>
                                         </div>
                                         <div class="bottombox clear" v-for="(good,index) in item.Goods" :key="index">
                                             <div class="infobox">
